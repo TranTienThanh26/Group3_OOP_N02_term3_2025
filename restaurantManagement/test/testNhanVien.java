f0908bc56746f496f285a6d6bf7643ac018786c2
TranTienThanh26 <151829060+TranTienThanh26@users.noreply.github.com>
2025-05-17 06:20:54 +0000

    nhanvien

--- restaurantManagement/test/testNhanVien.java
+++ restaurantManagement/test/testNhanVien.java
@@ -1,52 +1,12 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class testNhanVien {
-
-    @Test
-    public void testConstructorFull() {
+    public static void main(String[] args) {
         NhanVien nv = new NhanVien(1, "Nguyen Van A", "2023-01-01", "0123456789", "Nhan vien", 2, "Dang lam");
-
-        assertEquals(1, nv.getId_NV());
-        assertEquals("Nguyen Van A", nv.getTenNV());
-        assertEquals("2023-01-01", nv.getNgayVL());
-        assertEquals("0123456789", nv.getSdt());
-        assertEquals("Nhan vien", nv.getChucvu());
-        assertEquals(2, nv.getId_NQL());
-        assertEquals("Dang lam", nv.getTinhTrang());
-    }
-
-    @Test
-    public void testConstructorWithoutTinhTrang() {
-        NhanVien nv = new NhanVien(2, "Tran Thi B", "2024-01-01", "0987654321", "Quan ly", 3);
-
-        assertEquals(2, nv.getId_NV());
-        assertEquals("Tran Thi B", nv.getTenNV());
-        assertEquals("2024-01-01", nv.getNgayVL());
-        assertEquals("0987654321", nv.getSdt());
-        assertEquals("Quan ly", nv.getChucvu());
-        assertEquals(3, nv.getId_NQL());
-        assertNull(nv.getTinhTrang());
-    }
-
-    @Test
-    public void testSetterGetter() {
-        NhanVien nv = new NhanVien();
-
-        nv.setId_NV(3);
-        nv.setTenNV("Le Van C");
-        nv.setNgayVL("2025-05-16");
-        nv.setSdt("0111222333");
-        nv.setChucvu("Giam doc");
-        nv.setId_NQL(1);
-        nv.setTinhTrang("Nghi viec");
-
-        assertEquals(3, nv.getId_NV());
-        assertEquals("Le Van C", nv.getTenNV());
-        assertEquals("2025-05-16", nv.getNgayVL());
-        assertEquals("0111222333", nv.getSdt());
-        assertEquals("Giam doc", nv.getChucvu());
-        assertEquals(1, nv.getId_NQL());
-        assertEquals("Nghi viec", nv.getTinhTrang());
+        System.out.println("ID: " + nv.getId_NV());
+        System.out.println("Tên: " + nv.getTenNV());
+        System.out.println("Ngày vào làm: " + nv.getNgayVL());
+        System.out.println("SĐT: " + nv.getSdt());
+        System.out.println("Chức vụ: " + nv.getChucvu());
+        System.out.println("ID quản lý: " + nv.getId_NQL());
+        System.out.println("Tình trạng: " + nv.getTinhTrang());
     }
 }
